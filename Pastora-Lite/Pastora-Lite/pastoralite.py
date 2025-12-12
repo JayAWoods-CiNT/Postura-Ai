1fa158943963c87a0c26db10f8c1294177d216aa
JayAWoods-CiNT <jaysdubya@gmail.com>
2025-12-12 02:59:08 -0500

    Add PastoraLite class to load persona from JSON

--- Pastora-Lite/Pastora-Lite/pastoralite.py
+++ Pastora-Lite/Pastora-Lite/pastoralite.py
@@ -1,2 +1,9 @@
-{ … FULL persona.json FROM ABOVE … }
+import json
 
+class PastoraLite:
+    def __init__(self, persona_path="persona.json"):
+        with open(persona_path, "r", encoding="utf-8") as f:
+            self.persona = json.load(f)
+
+    def get_persona(self):
+        return self.persona
